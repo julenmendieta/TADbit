ebcbb5cbfffd3469feaa35cc11d712d42008ec33
fransua <serra.francois@gmail.com>
2014-11-12 20:39:50 +0100

    simplified and enhanced iterative mapping. added simple quality plot for fastq file

--- _pytadbit/_version.py
+++ _pytadbit/_version.py
@@ -1,1 +1,1 @@
-__version__ = "0.1_alpha.85"
+__version__ = "0.1_alpha.87"
--- _pytadbit/mapping/mapper.py
+++ _pytadbit/mapping/mapper.py
@@ -10,6 +10,137 @@ import tempfile
 import subprocess
 import gzip
 import pysam
+import gem
+
+def parse_sam(fnam):
+    """
+    """
+    reads1 = {}
+    reads2 = {}
+    nreads = {1:{}, 2:{}}
+    for read in [1, 2]:
+        reads = reads1 if read == 1 else reads2
+        for clen in range(20, 45, 5):
+            print 'loading length:', clen
+            for chunk in range(1, 3):
+                fnam = 'results_0/dmel_%s.txt.%s.%s' % (read, chunk, clen)
+                print ' ->', fnam
+                try:
+                    fhandler = pysam.Samfile(fnam)
+                except IOError:
+                    continue
+                for r in fhandler:
+                    if r.is_unmapped:
+                        continue
+                    if r.tags[1][1]!=1:
+                        continue
+                    positive = not r.is_reverse
+                    seq = r.seq
+                    qname = r.qname.split('.')[1]
+                    reads[qname] = (fhandler.getrname(r.tid), 
+                                    r.pos + (0 if positive else len(seq)),
+                                    positive, seq)
+            nreads[read].setdefault(clen, 0)
+            nreads[read][clen] += len(reads)
+    return reads1, reads2
+
+def trimming(raw_seq_len, seq_start, min_seq_len):
+    return seq_start, raw_seq_len - seq_start - min_seq_len
+
+
+def iterative_mapping(gem_index_path, fastq_path, out_sam_path,
+                      range_start, range_stop, **kwargs):
+    """
+    :param fastq_path: 152 bases first 76 from one end, next 76 from the other
+       end. Both to be read from left to right.
+    """
+    gem_index_path      = os.path.abspath(os.path.expanduser(gem_index_path))
+    fastq_path          = os.path.abspath(os.path.expanduser(fastq_path))
+    out_sam_path        = os.path.abspath(os.path.expanduser(out_sam_path))
+    single_end          = kwargs.get('single_end'          , False)
+    nthreads            = kwargs.get('nthreads'            , 4)
+    max_edit_distance   = kwargs.get('max_edit_distance'   , 0.04)
+    mismatches          = kwargs.get('mismatches'          , 0.04)
+    nthreads            = kwargs.get('nthreads'            , 4)
+    max_reads_per_chunk = kwargs.get('max_reads_per_chunk', -1)
+    temp_dir = os.path.abspath(os.path.expanduser(
+        kwargs.get('temp_dir', tempfile.gettempdir())))
+
+    #get the length of a read
+    fastqh = open(fastq_path)
+    raw_seq_len = int(fastqh.next().strip().split('length=')[1])
+    fastqh.close()
+
+    # Split input files if required and apply iterative mapping to each
+    # segment separately.
+    if max_reads_per_chunk > 0:
+        kwargs['max_reads_per_chunk'] = -1
+        print 'Split input file %s into chunks' % fastq_path
+        chunked_files = _chunk_file(
+            fastq_path,
+            os.path.join(temp_dir, os.path.split(fastq_path)[1]),
+            max_reads_per_chunk * 4)
+        print '%d chunks obtained' % len(chunked_files)
+        for i, fastq_chunk_path in enumerate(chunked_files):
+            print 'Run iterative_mapping recursively on %s' % fastq_chunk_path
+            iterative_mapping(
+                gem_index_path, fastq_chunk_path,
+                out_sam_path + '.%d' % (i + 1), range_start[:], range_stop[:],
+                **kwargs)
+
+            # Delete chunks only if the file was really chunked.
+            if len(chunked_files) > 1:
+                print 'Remove the chunks: %s' % ' '.join(chunked_files)
+                os.remove(fastq_chunk_path)
+        return
+
+    # end position according to sequence in the file
+    try:
+        seq_end = range_stop.pop(0)
+        seq_beg = range_start.pop(0)
+    except IndexError:
+        return
+
+    # define what we trim
+    seq_len = seq_end - seq_beg
+    trim_5, trim_3 = trimming(raw_seq_len, seq_beg, seq_len)
+
+    # output
+    local_out_sam = out_sam_path + '.%d' % (seq_len)
+    # input
+    inputf = gem.files.open(fastq_path)
+
+    # trimming
+    trimmed = gem.filter.run_filter(
+        inputf, ['--hard-trim', '%d,%d' % (trim_5, trim_3)],
+        threads=nthreads, paired=not single_end)
+    
+    # mapping
+    mapped = gem.mapper(trimmed, gem_index_path, min_decoded_strata=0,
+                        max_decoded_matches=2, unique_mapping=False,
+                        max_edit_distance=max_edit_distance,
+                        mismatches=mismatches,
+                        output=temp_dir + '/test.map',
+                        threads=nthreads)
+
+    # convert to sam
+    sam = gem.gem2sam(mapped, index=gem_index_path, output=local_out_sam,
+                      threads=nthreads, single_end=single_end)
+
+    # Check if the next iteration is required.
+    if not range_stop:
+        return
+
+    # Recursively go to the next iteration.
+    unmapped_fastq_path = os.path.join(
+        temp_dir, os.path.split(fastq_path)[1] + '.%d' % seq_len)
+    _filter_unmapped_fastq(fastq_path, local_out_sam, unmapped_fastq_path)
+
+    iterative_mapping(gem_index_path, unmapped_fastq_path,
+                      out_sam_path,
+                      range_start, range_stop, **kwargs)
+
+    os.remove(unmapped_fastq_path)
 
 
 def iterative_mapping_gem(gem_index_path, fastq_path, out_sam_path,
@@ -107,15 +238,14 @@ def iterative_mapping_gem(gem_index_path, fastq_path, out_sam_path,
     # run iterative_mapping recursively on the converted file.
     if bash_reader != 'cat':
         converted_fastq = os.path.join(temp_dir, os.path.split(fastq_path)[1] + '.fastq')
-        print ('Bash reader is not trivial, read input with %s, store in %s',
-               ' '.join(reading_command), converted_fastq)
+        print 'Bash reader is not trivial, read input with %s, store in %s' % (' '.join(reading_command), converted_fastq)
         converting_process = subprocess.Popen(
             reading_command,
             stdout=open(converted_fastq, 'w'))
         converting_process.wait()
 
         kwargs['bash_reader'] = 'cat'
-        print ('Run iterative_mapping recursively on %s', converted_fastq)
+        print 'Run iterative_mapping recursively on %s' % converted_fastq
         iterative_mapping_gem(
             gem_index_path, converted_fastq,
             out_sam_path, min_seq_len, len_step,
@@ -131,14 +261,14 @@ def iterative_mapping_gem(gem_index_path, fastq_path, out_sam_path,
     # segment separately.
     if max_reads_per_chunk > 0:
         kwargs['max_reads_per_chunk'] = -1
-        print ('Split input file %s into chunks', fastq_path)
+        print 'Split input file %s into chunks' % fastq_path
         chunked_files = _chunk_file(
             fastq_path,
             os.path.join(temp_dir, os.path.split(fastq_path)[1]),
             max_reads_per_chunk * 4)
-        log.debug('%d chunks obtained', len(chunked_files))
+        print '%d chunks obtained' % len(chunked_files)
         for i, fastq_chunk_path in enumerate(chunked_files):
-            print ('Run iterative_mapping recursively on %s', fastq_chunk_path)
+            print 'Run iterative_mapping recursively on %s' % fastq_chunk_path
             iterative_mapping_gem(
                 gem_index_path, fastq_chunk_path,
                 out_sam_path + '.%d' % (i + 1), min_seq_len, len_step,
@@ -146,7 +276,7 @@ def iterative_mapping_gem(gem_index_path, fastq_path, out_sam_path,
 
             # Delete chunks only if the file was really chunked.
             if len(chunked_files) > 1:
-                print ('Remove the chunks: %s', ' '.join(chunked_files))
+                print 'Remove the chunks: %s' % ' '.join(chunked_files)
                 os.remove(fastq_chunk_path)
         return
 
@@ -177,12 +307,11 @@ def iterative_mapping_gem(gem_index_path, fastq_path, out_sam_path,
         pipeline = []
         cutoff = 0.04
         try:
-            print ('Reading command: %s', ' '.join(reading_command))
+            print 'Reading command: %s' % ' '.join(reading_command)
             # pipeline.append(
             #     subprocess.Popen(reading_command, stdout=subprocess.PIPE))
             if not os.path.exists(local_out_sam):
                 sys.stderr.write('Mapping in ' + local_out_sam + '\n')
-                import gem
 
                 # inputf = gem.files.open(pipeline[-1].stdout)
                 inputf = gem.files.open(fastq_path)
@@ -218,8 +347,7 @@ def iterative_mapping_gem(gem_index_path, fastq_path, out_sam_path,
             return
 
         # Recursively go to the next iteration.
-        print ('Save unique aligments and send the '
-               'non-unique ones to the next iteration')
+        print 'Save unique aligments and send the non-unique ones to the next iteration'
 
         unmapped_fastq_path = os.path.join(
             temp_dir, os.path.split(fastq_path)[1] + '.%d' % min_seq_len)
@@ -232,6 +360,42 @@ def iterative_mapping_gem(gem_index_path, fastq_path, out_sam_path,
 
         os.remove(unmapped_fastq_path)
 
+
+def _line_count(path):
+    '''Count the number of lines in a file. The function was posted by
+    Mikola Kharechko on Stackoverflow.
+    '''
+
+    f = open(path)
+    lines = 0
+    buf_size = 1024 * 1024
+    read_f = f.read  # loop optimization
+
+    buf = read_f(buf_size)
+    while buf:
+        lines += buf.count('\n')
+        buf = read_f(buf_size)
+
+    return lines
+
+def _chunk_file(in_path, out_basename, max_num_lines):
+    '''Slice lines from a large file.
+    The line numbering is as in Python slicing notation.
+    '''
+    num_lines = _line_count(in_path)
+    if num_lines <= max_num_lines:
+        return [in_path, ]
+
+    out_paths = []
+
+    for i, line in enumerate(open(in_path)):
+        if i % max_num_lines == 0:
+            out_path = out_basename + '.%d' % (i // max_num_lines + 1)
+            out_paths.append(out_path)
+            out_file = file(out_path, 'w')
+        out_file.write(line)
+
+    return out_paths
 
 def _filter_fastq(ids, in_fastq, out_fastq):
     '''Filter FASTQ sequences by their IDs.
